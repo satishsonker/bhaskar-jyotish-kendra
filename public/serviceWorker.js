72e804a7acb1c76afd6f587ae2de823ce9dab794
Satish Kumar Sonker <btech.csit@gmail.com>
2025-04-04 13:43:06 +0530

    Delete All Files

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -5,7 +5,15 @@ const urlsToCache = [
   '/static/js/main.bundle.js',
   '/static/css/main.bundle.css',
   '/logo.svg',
-  '/manifest.json'
+  '/manifest.json',
+  '/icons/icon-72x72.png',
+  '/icons/icon-96x96.png',
+  '/icons/icon-128x128.png',
+  '/icons/icon-144x144.png',
+  '/icons/icon-152x152.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-384x384.png',
+  '/icons/icon-512x512.png'
 ];
 
 // Install a service worker
@@ -17,6 +25,7 @@ self.addEventListener('install', event => {
         return cache.addAll(urlsToCache);
       })
   );
+  self.skipWaiting();
 });
 
 // Cache and return requests
@@ -28,9 +37,27 @@ self.addEventListener('fetch', event => {
         if (response) {
           return response;
         }
-        return fetch(event.request);
-      }
-    )
+
+        // Clone the request because it's a stream and can only be consumed once
+        const fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(response => {
+          // Check if we received a valid response
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+
+          // Clone the response because it's a stream and can only be consumed once
+          const responseToCache = response.clone();
+
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+
+          return response;
+        });
+      })
   );
 });
 
@@ -48,4 +75,5 @@ self.addEventListener('activate', event => {
       );
     })
   );
+  self.clients.claim();
 }); 
